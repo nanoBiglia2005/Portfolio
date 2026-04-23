ea364ecfeba8d4cff6fea73e02a78f565737d90a
nanoBiglia2005 <bigliastefano2005@gmail.com>
2026-04-23 15:11:31 -0300

    modified:   package-lock.json 	modified:   package.json 	modified:   src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 export default function Home() {
   return <div>Hola mundo!</div>;

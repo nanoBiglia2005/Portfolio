a104e7b391b4ecfebfdee8aacf9fcba9006be32d
nanoBiglia2005 <bigliastefano2005@gmail.com>
2026-04-23 17:11:49 -0300

    modified:   src/app/globals.css 	modified:   src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,19 @@ import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 export default function Home() {
-  return <div>Hola mundo!</div>;
+  return <div>
+    <header> Github </header>
+    <main style={{display: "flex", flexDirection: "row"}}>
+      <div style={{ display: "flex", flexDirection: "column"}}>
+        <h1 style={{ color: "white", fontSize: "5rem", fontWeight: "bold"}}> Stefano Biglia </h1>
+        <p style={{ color: "white", fontSize: "1.5rem", fontWeight: "bold"}}> Técnico en Programación </p>
+      </div>
+      <div>
+        <div>
+          Sobre Mí          
+        </div>
+      </div>
+    </main>
+  
+    </div>;
 }
